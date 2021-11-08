f4c92a6d35aa9edcb67fd42e83aaecaf465ce114
alsomeb <alsomeb@gmail.com>
2021-11-08 12:34:42 +0100

    updated

--- main.py
+++ main.py
@@ -11,13 +11,17 @@ while True:
     print("\n")
     res = manager.GetRows(country_name, prio)
 
-    # Lägger in objekten
-    for row in res: #måste splitta här också, för listan är bara en full rad med en string
-        res = row.split(",")
-        manager.addSale(res[0],res[1],res[2],res[3],res[4],res[5],res[6],res[7],res[8],res[9],res[10],res[11],res[12],res[13])
-    print(f"Total Revenue: {round(manager.calcTotalRevenue(), 2)}")
-    print(f"Total Cost: {round(manager.calcTotalCost(), 2)}")
-    print(f"Total Profit: {round(manager.calcTotalProfit(), 2)}")
-    print("\n")
+    if not res: # empty lists are considered "false" in python, thus IF NOT
+        print("No sales matching your inputs.\n")
+
+    else:
+        # Lägger in objekten
+        for row in res: #måste splitta här också, för listan är bara en full rad med en string
+            res = row.split(",")
+            manager.addSale(res[0],res[1],res[2],res[3],res[4],res[5],res[6],res[7],res[8],res[9],res[10],res[11],res[12],res[13])
+        print(f"Total Revenue: {round(manager.calcTotalRevenue(), 2)}")
+        print(f"Total Cost: {round(manager.calcTotalCost(), 2)}")
+        print(f"Total Profit: {round(manager.calcTotalProfit(), 2)}")
+        print("\n")
 
 ##test
