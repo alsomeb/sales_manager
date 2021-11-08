38596ccb56b47e66664dd0570f572bdbbe7fca76
alsomeb <alsomeb@gmail.com>
2021-11-08 12:46:47 +0100

    updated2

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ while True:
     else:
         # Lägger in objekten
         for row in res: #måste splitta här också, för listan är bara en full rad med en string
+            print(f"{row}\n")
             res = row.split(",")
             manager.addSale(res[0],res[1],res[2],res[3],res[4],res[5],res[6],res[7],res[8],res[9],res[10],res[11],res[12],res[13])
         print(f"Total Revenue: {round(manager.calcTotalRevenue(), 2)}")
