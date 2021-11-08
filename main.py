3a2df7a36a845882a2a0910b52226e70e4cfe496
alsomeb <alsomeb@gmail.com>
2021-11-08 12:27:16 +0100

    update

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ while True:
     res = manager.GetRows(country_name, prio)
 
     # Lägger in objekten
-    for row in res:
+    for row in res: #måste splitta här också, för listan är bara en full rad med en string
         res = row.split(",")
         manager.addSale(res[0],res[1],res[2],res[3],res[4],res[5],res[6],res[7],res[8],res[9],res[10],res[11],res[12],res[13])
     print(f"Total Revenue: {round(manager.calcTotalRevenue(), 2)}")
