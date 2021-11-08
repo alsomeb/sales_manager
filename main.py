74610f5af0fce4560626a3cfa918aad7c89845e2
alsomeb <alsomeb@gmail.com>
2021-11-08 12:51:39 +0100

    updated3

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ while True:
             print(f"{row}\n")
             res = row.split(",")
             manager.addSale(res[0],res[1],res[2],res[3],res[4],res[5],res[6],res[7],res[8],res[9],res[10],res[11],res[12],res[13])
-        print(f"Total Revenue: {round(manager.calcTotalRevenue(), 2)}")
-        print(f"Total Cost: {round(manager.calcTotalCost(), 2)}")
-        print(f"Total Profit: {round(manager.calcTotalProfit(), 2)}")
+        print(f"Total Revenue: {round(manager.calcTotalRevenue(), 1)}")
+        print(f"Total Cost: {round(manager.calcTotalCost(), 1)}")
+        print(f"Total Profit: {round(manager.calcTotalProfit(), 1)}")
         print("\n")
 
 ##test
